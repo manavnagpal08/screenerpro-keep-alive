81045cbe81df7e3fcd5c564a4c5c76632095dd94
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-07-27 16:17:33 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,10 +1,14 @@
 import requests
 from datetime import datetime
 
-url = "https://candidate-screeneerpro.streamlit.app/"
+urls = [
+    "https://candidate-screeneerpro.streamlit.app/",
+    "https://screenerpro.streamlit.app/"
+]
 
-try:
-    res = requests.get(url)
-    print(f"[{datetime.now()}] Pinged ScreenerPro - Status {res.status_code}")
-except Exception as e:
-    print(f"[{datetime.now()}] Error: {e}")
+for url in urls:
+    try:
+        res = requests.get(url)
+        print(f"[{datetime.now()}] Pinged {url} - Status: {res.status_code}")
+    except Exception as e:
+        print(f"[{datetime.now()}] Error pinging {url}: {e}")
